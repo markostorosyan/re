a3a043c1b66e849b1bc95ccde1a63c2780171ea4
markostorosyan <104504002+markostorosyan@users.noreply.github.com>
2022-10-10 22:33:44 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+let arr = [];
+let foundCorrectNumber = false;
+let life = 3
+let num = Math.floor(Math.random() * 11)
+let wordArray = ['haaaaaaaaaaa', 'taa', 'caaaa']
+let one;
+
+while(true) {
+  if(wordArray[num] !== undefined) {
+      one = wordArray[num].split('')
+      break
+  }
+  num = Math.floor(Math.random() * 11)
+}
+
+let two = one.join('')
+console.log("You have 3 life")
+
+while (!foundCorrectNumber) {
+  let guess = prompt('say word: ');
+  
+  if(guess === (one.find((val) => {if(val === guess) {return val}}))) {
+
+    for(let j = 0; j < one.length; j++) {
+      if(one[j] === guess) {
+        arr[j] = guess
+        console.log(arr);
+        delete one[j]
+      }
+    }
+    if(two === arr.join('')) {
+      console.log('you win');
+      foundCorrectNumber = true;
+    }
+  } else {
+    life = life - 1
+    console.log(`You have left ${life} life`)
+    if(life === 0) {
+      console.log('You louse :P')
+      foundCorrectNumber = true;
+    }
+  }
+}
